6ac264392a39c2204844c93702f5cd9555349787
Jason Mayo <jason@bymayo.co.uk>
2018-06-04 11:32:24 +0100

    Removed vendor folder

--- vendor/craftcms/cms/src/config/cproutes/pro.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'users/new' => 'users/edit-user',
-    'users/<userId:\d+>' => 'users/edit-user',
-    'settings/users' => ['template' => 'settings/users/groups/_index'],
-    'settings/users/groups/new' => ['template' => 'settings/users/groups/_edit'],
-    'settings/users/groups/<groupId:\d+>' => ['template' => 'settings/users/groups/_edit'],
-];
