f85e612d13a0e35954d53a73f4c75f67f7ebf43f
Jason Mayo <jason@bymayo.co.uk>
2018-06-05 11:57:58 +0100

    Asset Rev config and base

--- config/assetrev.php
+++ config/assetrev.php
@@ -1,6 +1,6 @@
 <?php
    return array(
-       'manifestPath' => 'rev.json',
+       'manifestPath' => '../rev-manifest.json',
        'assetsBasePath' => '../web/dist/',
        'assetUrlPrefix' => getenv('ASSET_URL_PREFIX'),
    );
