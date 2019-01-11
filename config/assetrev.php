429eb89501ecd2f9d4e3856a7ba9e935b4a60ba5
Jason Mayo <jason@bymayo.co.uk>
2019-01-11 10:59:18 +0000

    Removed asset rev from staging

--- config/assetrev.php
+++ config/assetrev.php
@@ -1,10 +1,6 @@
 <?php
    return array(
       '*' => array(),
-      'staging' => array(
-         'manifestPath' => 'rev-manifest.json',
-         'assetsBasePath' => '../web/dist/'
-      ),
       'production' => array(
          'manifestPath' => 'rev-manifest.json',
          'assetsBasePath' => '../web/dist/'
