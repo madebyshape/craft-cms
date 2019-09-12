3794fd9404c0b4a38d8d9426c4cd6507e4a1c6f9
Jason Mayo <jason@bymayo.co.uk>
2019-09-12 13:44:46 +0100

    Added Freeform Config and removed CPNav

---
+++ config/freeform.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Freeform Configuration
+ */
+
+return [
+    '*' => [
+        'pluginName' => 'Forms',
+        'showTutorial' => false,
+        'fieldDisplayOrder'  => \Solspace\Freeform\Freeform::FIELD_DISPLAY_ORDER_NAME,
+        'defaultTemplates' => false,
+        'formTemplateDirectory' => '_plugins/freeform',
+    ]
+];
