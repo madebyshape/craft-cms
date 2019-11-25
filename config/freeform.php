cf51257f3417474267ede91ba314ab6f81bff69f
Jason Mayo <jason@bymayo.co.uk>
2019-11-25 15:57:19 +0000

    Enabled default templates in freeform

--- config/freeform.php
+++ config/freeform.php
@@ -9,7 +9,6 @@ return [
         'pluginName' => 'Forms',
         'showTutorial' => false,
         'fieldDisplayOrder'  => \Solspace\Freeform\Freeform::FIELD_DISPLAY_ORDER_NAME,
-        'defaultTemplates' => false,
         'formTemplateDirectory' => '_plugins/freeform',
     ]
 ];
