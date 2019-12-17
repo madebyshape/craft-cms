9c97e350f5777daaefd471fef6799a8b2eb13119
Jason Mayo <jason@bymayo.co.uk>
2019-12-17 12:11:02 +0000

    Added blitz.php to /config

---
+++ config/blitz.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @copyright Copyright (c) PutYourLightsOn
+ */
+
+/**
+ * Blitz config.php
+ *
+ * This file exists only as a template for the Blitz settings.
+ * It does nothing on its own.
+ *
+ * Don't edit this file, instead copy it to 'craft/config' as 'blitz.php'
+ * and make your changes there to override default settings.
+ *
+ * Once copied to 'craft/config', this file will be multi-environment aware as
+ * well, so you can have different settings groups for each environment, just as
+ * you do for 'general.php'
+ */
+
+return [
+    '*' => [
+        'cachingEnabled' => true,
+        'includedUriPatterns' => [
+            [
+                'siteId' => '',
+                'uriPattern' => '.*',
+            ]
+        ],
+        'excludedUriPatterns' => [],
+        'cacheStorageSettings' => ['folderPath' => '@webroot/cache/blitz'],
+        'clearCacheAutomatically' => true,
+        'warmCacheAutomatically' => true,
+        'refreshCacheAutomaticallyForGlobals' => true
+    ],
+    'dev' => [
+        'cachingEnabled' => false
+    ],
+    'staging' => [
+    ],
+    'production' => [
+    ]
+];
