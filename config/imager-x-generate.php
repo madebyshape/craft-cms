26deba275573f7b337fd0f15c07b07f3e0805f35
Jason Mayo <jason@bymayo.co.uk>
2020-02-12 16:33:36 +0000

    Installed Imager X

---
+++ config/imager-x-generate.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+//    'elements' => [
+//       [
+//           'elementType' =>  \craft\elements\Entry::class,
+//           'criteria' => [
+//               'section' => 'news',
+//           ],
+//           'fields' => ['image', 'articleBlocks:images.images'],
+//           'transforms' => ['heroImage', 'listImage', 'metaImage']
+//       ]
+//   ]
+];
---
+++ config/imager-x-transforms.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+   //  'example' => [
+   //      'transforms' => [
+   //          ['width' => 600],
+   //          ['width' => 1800],
+   //      ],
+   //      'defaults' => [
+   //          'ratio' => 16/9,
+   //          'jpegQuality' => 80
+   //      ],
+   //      'configOverrides' => [
+   //          'fillTransforms' => true,
+   //          'fillInterval' => 300,
+   //      ]
+   //  ]
+];
