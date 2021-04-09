2c4bb460476f9a7e6f1773dd183305fbed68df7f
Jason Mayo <jason@bymayo.co.uk>
2021-04-09 11:13:27 +0100

    Added: preserveColorProfiles to Imager X

--- config/imager-x.php
+++ config/imager-x.php
@@ -8,6 +8,7 @@
          'cacheEnabled' => true,
          'cacheDuration' => 31536000, // 1 year
          'cacheDurationRemoteFiles' => 31536000, // 1 year
+         'preserveColorProfiles' => true,
          'jpegQuality' => 90,
          'pngCompressionLevel' => 0,
          'resizeFilter' => 'lanczos',
