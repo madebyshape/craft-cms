f9f9cf6bc0e3bead6f66efb5e84c72df65a68fc7
Jason Mayo <jason@bymayo.co.uk>
2022-06-29 11:15:39 +0100

    Removed: Old framework

--- config/assetrev.php
+++
@@ -1,8 +0,0 @@
-<?php
-   return array(
-      '*' => array(),
-      'production' => array(
-         'manifestPath' => 'rev-manifest.json',
-         'assetsBasePath' => '../' . getenv('PUBLIC_FOLDER') . '/dist/'
-      )
-   );
--- config/minify.php
+++
@@ -1,4 +0,0 @@
-<?php
-   return array(
-      'disableDevmodeMinifying' => true
-   );
