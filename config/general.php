e7c00efc404ccf0562489a8d278ffe9d00286cea
Jason Mayo <jason@bymayo.co.uk>
2022-06-29 11:28:17 +0100

    Added: ddev & Craft 4

---
+++ config/general.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * General Configuration
+ *
+ * All of your system's general configuration settings go in here. You can see a
+ * list of the available settings in vendor/craftcms/cms/src/config/GeneralConfig.php.
+ *
+ * @see \craft\config\GeneralConfig
+ */
+
+use craft\helpers\App;
+
+$isDev = App::env('CRAFT_ENVIRONMENT') === 'dev';
+$isProd = App::env('CRAFT_ENVIRONMENT') === 'production';
+
+return [
+    // Default Week Start Day (0 = Sunday, 1 = Monday...)
+    'defaultWeekStartDay' => 1,
+
+    // Whether generated URLs should omit "index.php"
+    'omitScriptNameInUrls' => true,
+
+    // Whether Dev Mode should be enabled (see https://craftcms.com/guides/what-dev-mode-does)
+    'devMode' => $isDev,
+
+    // Whether administrative changes should be allowed
+    'allowAdminChanges' => $isDev,
+
+    // Whether crawlers should be allowed to index pages and following links
+    'disallowRobots' => !$isProd,
+];
