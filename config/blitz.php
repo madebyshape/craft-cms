0b79c9f929e1d7424a9a319d583237a7d818a5b7
Jason Mayo <jason@bymayo.co.uk>
2023-07-25 12:52:53 +0100

    Changed: Enabled Blitz on Staging and added includedQueryStringParams

--- config/blitz.php
+++ config/blitz.php
@@ -19,23 +19,34 @@
 
 return [
     '*' => [
-        'cachingEnabled' => false,
+        'cachingEnabled' => true,
+        'cacheStorageType' => 'putyourlightson\blitz\drivers\storage\YiiCacheStorage',
         'includedUriPatterns' => [
             [
                 'uriPattern' => '.*',
             ]
         ],
         'excludedUriPatterns' => [
-            ['uriPattern' => 'dynamic/form']
+            ['uriPattern' => 'dynamic/.*']
         ],
-        // 'cacheStorageType' => 'putyourlightson\blitz\drivers\storage\YiiCacheStorage',
-        'cacheStorageSettings' => ['folderPath' => '@root/cache/blitz'],
+        'includedQueryStringParams' => [
+            [
+                'uriPattern' => '.*',
+            ]
+         ],
         'clearCacheAutomatically' => true,
         'warmCacheAutomatically' => false,
         'refreshCacheAutomaticallyForGlobals' => true,
+        'queueJobTtr' => 600, // 10 mins
         'queryStringCaching' => 1
     ],
+    'dev' => [
+        'cachingEnabled' => false,
+        'cacheStorageSettings' => ['folderPath' => '@root/cache/blitz'],
+        'cacheStorageType' => 'putyourlightson\blitz\drivers\storage\FileStorage',
+    ],
+    'staging' => [
+    ],
     'production' => [
-        'cachingEnabled' => true
     ]
 ];
