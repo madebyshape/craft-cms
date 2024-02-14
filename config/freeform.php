05c9e6130ca7ea755a6c3118b8339e3f0828e0eb
Jason Mayo <jason@bymayo.co.uk>
2024-02-14 15:43:47 +0000

    Removed: Freeform field order due to deprecation

--- config/freeform.php
+++ config/freeform.php
@@ -8,7 +8,6 @@ return [
     '*' => [
         'pluginName' => 'Forms',
         'showTutorial' => false,
-        'fieldDisplayOrder'  => \Solspace\Freeform\Freeform::FIELD_DISPLAY_ORDER_NAME,
         'formTemplateDirectory' => '_plugins/freeform',
     ]
 ];
