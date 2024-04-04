42a0c431adbcd703afc6a29754907343125bd21d
Jason Mayo <jason@bymayo.co.uk>
2024-04-04 11:23:34 +0100

    Added: Craft 5 framework

---
+++ config/general.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * General Configuration
+ *
+ * All of your system's general configuration settings go in here. You can see a
+ * list of the available settings in vendor/craftcms/cms/src/config/GeneralConfig.php.
+ *
+ * @see \craft\config\GeneralConfig
+ */
+
+use craft\config\GeneralConfig;
+use craft\helpers\App;
+
+return GeneralConfig::create()
+    // Set the default week start day for date pickers (0 = Sunday, 1 = Monday, etc.)
+    ->defaultWeekStartDay(1)
+    // Prevent generated URLs from including "index.php"
+    ->omitScriptNameInUrls()
+    // Preload Single entries as Twig variables
+    ->preloadSingles()
+    // Prevent user enumeration attacks
+    ->preventUserEnumeration()
+    // Set the @webroot alias so the clear-caches command knows where to find CP resources
+    ->aliases([
+        '@webroot' => dirname(__DIR__) . '/web',
+    ])
+;
---
+++ config/routes.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Site URL Rules
+ *
+ * You can define custom site URL rules here, which Craft will check in addition
+ * to routes defined in Settings → Routes.
+ *
+ * Read all about Craft’s routing behavior, here:
+ * https://craftcms.com/docs/4.x/routing.html
+ */
+
+return [];
