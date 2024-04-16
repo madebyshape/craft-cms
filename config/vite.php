96128c9f89cb200f06428540b07bf3b3430504d5
Jason Mayo <jason@bymayo.co.uk>
2024-04-16 16:23:47 +0100

    Fixed: Issue where server adds multiple ports

--- config/vite.php
+++ config/vite.php
@@ -6,7 +6,7 @@ return [
     'useDevServer' => App::env('ENVIRONMENT') === 'dev' || App::env('CRAFT_ENVIRONMENT') === 'dev',
     'manifestPath' => '@webroot/dist/manifest.json',
     'devServerInternal' => 'http://localhost:3000/',
-    'devServerPublic' => App::env('PRIMARY_SITE_URL') . ':3000',
+    'devServerPublic' => preg_replace('/:\d+/', '', App::env('PRIMARY_SITE_URL')) . ':3000',
     'serverPublic' => App::env('PRIMARY_SITE_URL') . '/dist/',
     'checkDevServer' => true,
     // 'criticalPath' => '@webroot/dist/criticalcss',
