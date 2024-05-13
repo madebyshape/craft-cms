07662ed9076935f67a03eb1fcaee6d56ffaac551
Jason Mayo <jason@bymayo.co.uk>
2024-05-13 12:08:12 +0100

    Fixed: Servd config issues

--- config/servd-asset-storage.php
+++ config/servd-asset-storage.php
@@ -5,7 +5,7 @@ use craft\helpers\App;
 return [
     'projectSlug' => App::env('SERVD_PROJECT_SLUG'),
     'securityKey' => App::env('SERVD_SECURITY_KEY'),
+    'assetsEnvironment' => App::env('SERVD_ASSET_ENVIRONMENT') ?? 'staging',
     'injectCors' => 1,
-    'clearCachesOnSave' => 'never',
-    'assetsEnvironment' => 'production'
+    'clearCachesOnSave' => 'never'
 ];
