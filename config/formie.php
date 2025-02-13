6e83adb43b3a92cf71bb23c47672bd5f334d152e
Jason Mayo <jason@bymayo.co.uk>
2025-02-13 10:49:57 +0000

    Added: Disabled fields to formie

--- config/formie.php
+++ config/formie.php
@@ -2,6 +2,29 @@
 
 return [
     '*' => [
-        'pluginName' => 'Forms'
+        'pluginName' => 'Forms',
+        'disabledFields' => [
+            'verbb\formie\fields\Address',
+            'verbb\formie\fields\Agree',
+            'verbb\formie\fields\Calculations',
+            'verbb\formie\fields\Categories',
+            'verbb\formie\fields\Date',
+            'verbb\formie\fields\Entries',
+            'verbb\formie\fields\FileUpload',
+            'verbb\formie\fields\Group',
+            'verbb\formie\fields\Heading',
+            'verbb\formie\fields\Name',
+            'verbb\formie\fields\Payment',
+            'verbb\formie\fields\Password',
+            'verbb\formie\fields\Phone',
+            'verbb\formie\fields\Repeater',
+            'verbb\formie\fields\Recipients',
+            'verbb\formie\fields\Section',
+            'verbb\formie\fields\Signature',
+            'verbb\formie\fields\Summary',
+            'verbb\formie\fields\Table',
+            'verbb\formie\fields\Tags',
+            'verbb\formie\fields\Users',
+        ],
     ]
 ];
